091b74e5305c77f2abc63b1f3f00b7da69d25426
klvlo <jejuhjs@gmail.com>
2024-02-04 00:36:51 +0900

    feat: 회원가입(네이버X)

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,12 @@
 import { addUser } from "../models/user.dao.js";
 import { status } from "../../config/response.status.js";
+import { BaseError } from "../../config/error.js";
 
 export const joinUser = async (body) => {
     try {
         // 비밀번호와 비밀번호 확인 일치 여부 확인
         if (body.pw !== body.pwcheck) {
-            throw new Error("비밀번호가 일치하지 않습니다.");
+            throw new BaseError(status.BAD_REQUEST);
         }
 
         const joinUserData = await addUser({
@@ -20,11 +21,11 @@ export const joinUser = async (body) => {
         });
 
         if (joinUserData === -1) {
-            throw new Error("이미 존재하는 이메일입니다.");
+            throw new BaseError(status.BAD_REQUEST);
         } else {
             // 회원가입 성공 시 응답 데이터 구성 (사용자 정보 반환하지 않음)
             const successMessage = "회원가입에 성공했습니다."; // 성공 메시지 추가
-            return { success: true, message: successMessage };
+            return response(status.SUCCESS, null);
         }
     } catch (error) {
         // 예외 처리
