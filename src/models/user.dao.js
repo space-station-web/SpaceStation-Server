fedc0e32407a5a0079ea3f4afbd7625a2c05675c
klvlo <jejuhjs@gmail.com>
2024-02-11 00:03:05 +0900

    feat:회원가입기능

--- src/models/user.dao.js
+++ src/models/user.dao.js
@@ -1,6 +1,6 @@
 import { pool } from "../../config/db.config.js";
 import { status } from "../../config/response.status.js";
-import { emailcheckSql, userCheckSql, confirmEmailSql, confirmNickname, insertUserSql } from "./user.sql.js";
+import { emailcheckSql, userCheckSql, confirmEmailSql, confirmNicknameSql, insertUserSql } from "./user.sql.js";
 import jwtUtil from "../../config/jwt-util.js";
 import crypto from 'crypto';
 import dotenv from 'dotenv';
@@ -77,7 +77,6 @@ export const checkNicknameDuplication = async (nickname) => {
         return '서버 에러'; // 또는 다른 적절한 에러를 throw
     }
 };
-import { emailcheckSql } from "./user.sql.js";
 
 
 export const findemail = async (data) => {
@@ -151,70 +150,5 @@ export const logintry = async (data) => {
     } catch (err) {
         console.error(err); // 에러 출력
         return -1;
-    }
-};
-
-export const addUser = async (data) => {
-    try {
-        const conn = await pool.getConnection();
-
-        // 이메일 중복 확인
-        const [emailConfirm] = await pool.query(confirmEmailSql, data.email);
-
-        if (emailConfirm[0].isExistEmail) {
-            conn.release();
-            return -1; // 이메일 중복일 경우 -1 반환 또는 다른 적절한 값을 선택
-        }
-
-        const salt = crypto.randomBytes(128).toString('base64');
-        const hashedPw = crypto
-            .createHash(createdHash)
-            .update(data.pw + salt)
-            .digest(digest);
-
-        // 사용자 데이터 삽입
-        const result = await pool.query(insertUserSql, [
-            data.name,
-            data.nickname,
-            data.email,
-            hashedPw,
-            data.phone,
-            data.b_date,
-            data.alarm, // 약관 동의 여부
-            new Date(), // created
-            'active', // status
-            'local',// provider
-            salt
-        ]);
-
-        conn.release();
-
-        // 반환값을 사용자 닉네임으로 변경
-        return "회원가입이 완료되었습니다.";
-    } catch (err) {
-        console.error(err); // 에러 출력
-        return "서버에러"; // 또는 다른 적절한 값을 반환
-    }
-};
-
-export const checkNicknameDuplication = async (nickname) => {
-    try {
-        const conn = await pool.getConnection();
-
-        // 닉네임 중복 확인
-        const [nicknameConfirm] = await conn.query(confirmNickname, [nickname]);
-        if( nicknameConfirm[0].isExistNickname) {
-            conn.release()
-            console.log("닉네임 중복")
-            return -1;
-        } else {
-            conn.release()
-
-            console.log("사용가능");
-            return "사용 가능한 닉네임입니다.";
-        }
-    } catch (error) {
-        console.error(error);
-        return '서버 에러'; // 또는 다른 적절한 에러를 throw
     }
 };
