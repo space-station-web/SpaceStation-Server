9c76ee4eea5823839b41a46d605620875beb2f86
klvlo <jejuhjs@gmail.com>
2024-02-12 21:52:17 +0900

    feat:비밀번호변경기능

--- src/routes/emailcheck.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import {  } from "../controllers/emailcheck.controller.js";
-
-export const emailauthRouter = express.Router();
-
-emailauthRouter.post('/', emailauth)
