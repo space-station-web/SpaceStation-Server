c6b0f400587446e438f9626aefe1a45b6124ef7f
yoolha <dbstjwls0247@naver.com>
2024-02-15 18:29:51 +0900

    feat: 글 상세  조회 유저 검증 삭제

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -24,7 +24,8 @@ postRouter.get('/follow-posts', tokenChecker, asyncHandler(getFollowPosts));
 // postRouter.get('/follow-posts', asyncHandler(getFollowPosts)); // 미들웨어 사용안한것
 
 // 글 조회
-postRouter.get('/:post_id', tokenChecker, asyncHandler(getPost));
+postRouter.get('/:post_id', asyncHandler(getPost));
+// postRouter.get('/:post_id', tokenChecker, asyncHandler(getPost));
 
 // 글 수정
 postRouter.patch('/:post_id', tokenChecker, asyncHandler(editPost));
