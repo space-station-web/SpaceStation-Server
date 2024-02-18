10cc26ef6e6a7623a2a4b6a4e84ba35302767ce7
yoolha <dbstjwls0247@naver.com>
2024-02-19 00:53:16 +0900

    feat: 답변 수정, 삭제 구현

--- src/routes/question.route.js
+++ src/routes/question.route.js
@@ -2,7 +2,7 @@
 
 import express from "express";
 import asyncHandler from 'express-async-handler';
-import { postAnswer, todayAnswer, todayQuestion, getMyAnswer } from '../controllers/question.controller.js';
+import { postAnswer, todayAnswer, todayQuestion, getMyAnswer, editAnswer, delAnswer } from '../controllers/question.controller.js';
 import { tokenChecker } from "../../config/jwt-util.js";
 
 export const questionRouter = express.Router();
@@ -15,3 +15,7 @@ questionRouter.get('/my-answer', tokenChecker, asyncHandler(getMyAnswer));
 questionRouter.post('/', tokenChecker, asyncHandler(postAnswer));
 
 questionRouter.get('/:question_id', tokenChecker, asyncHandler(todayAnswer));
+
+questionRouter.patch('/:answer_id', tokenChecker, asyncHandler(editAnswer));
+
+questionRouter.delete('/:answer_id', tokenChecker, asyncHandler(delAnswer));
